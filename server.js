4519b80a689bd17aa167fa49ff21be0a54711595
Carlos Godoy <cgod720@gmail.com>
2019-05-01 00:27:29 -0400

    added cors

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors')
 const PORT = process.env.PORT || 5000;
 
 //Database
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/bloggin';
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+  credentials: true
+}
+
 //Middleware
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cors());
 
 //Routes Controllers
 const entryController = require('./controllers/entry.js');
-app.use('/entry', entryController)
+app.use('/entry', cors(), entryController)
 
 //Listener
 app.listen(PORT, () => {
